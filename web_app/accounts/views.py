6c95c876315f057bb89de34e2dbe237ffd9e4649
CHH <chh2126@gmail.com>
2021-02-09 16:33:58 +0900

    1차수정

--- web_app/accounts/views.py
+++ web_app/accounts/views.py
@@ -19,7 +19,6 @@ class UserRegisterView(CreateView):
     form_class = SignUpForm
     success_url = reverse_lazy('login')
 
-<<<<<<< HEAD
 class UserEditView(UpdateView):
     template_name = 'registration/edit_profile.html'
     form_class = EditProfileForm
@@ -27,20 +26,3 @@ class UserEditView(UpdateView):
 
     def get_object(self):
         return self.request.user
-=======
-# Create your views here.
-
-def register(request):
-    if request.method == "POST":
-        user_form = RegisterForm(request.POST)
-        if user_form.is_valid():
-            new_user = user_form.save(commit=False)
-            new_user.set_password(user_form.cleaned_data['password'])
-            new_user.save()
-            return render(request, 'registration/register_done.html',{'new_user':new_user})
-
-    else:
-        user_form = RegisterForm()
-    return render(request, 'registration/register.html', {'form':user_form})
-
->>>>>>> origin/web_list
