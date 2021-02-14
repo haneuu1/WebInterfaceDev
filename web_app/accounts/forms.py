30ea7dab28a2a473a38623f855c118abe9464b7c
CHH <chh2126@gmail.com>
2021-02-15 03:10:13 +0900

    phone, address추가

--- web_app/accounts/forms.py
+++ web_app/accounts/forms.py
@@ -1,17 +1,57 @@
+from django.contrib.auth.forms import PasswordChangeForm, UserChangeForm, UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
+from django.forms import fields
+from .models import Profile
 
-class RegisterForm(forms.ModelForm):
-    password = forms.CharField(label='Password', widget=forms.PasswordInput)
-    password2 = forms.CharField(label='Repeat Password', widget=forms.PasswordInput)
+class SignUpForm(UserCreationForm):
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control'}))
+    first_name = forms.CharField(max_length=20, widget=forms.TextInput(attrs={'class':'form-control'}))
+    last_name = forms.CharField(max_length=20, widget=forms.TextInput(attrs={'class':'form-control'}))
 
     class Meta:
         model = User
-        # fields = '__all__'
-        fields = ['username', 'last_name', 'first_name', 'email']
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
 
-    def clean_password2(self):
-        cd = self.cleaned_data
-        if cd['password'] != cd['password2']:
-            raise forms.ValidationError('Passwords not matched!')
-        return cd['password2']
+    def __init__(self, *args, **kwargs):
+        super(SignUpForm, self).__init__(*args, **kwargs)
+
+        self.fields['username'].widget.attrs['class'] = 'form-control'
+        self.fields['password1'].widget.attrs['class'] = 'form-control'
+        self.fields['password2'].widget.attrs['class'] = 'form-control'
+
+class ProfileForm(forms.ModelForm):
+    phone = forms.CharField(max_length=20, widget=forms.TextInput(attrs={'class':'form-control'}))
+    address = forms.CharField(max_length=20, widget=forms.TextInput(attrs={'class':'form-control'}))
+    class Meta:
+        model = Profile
+        fields = ['phone', 'address']
+        
+
+class EditProfileForm(UserChangeForm):
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control'}))
+    first_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class':'form-control'}))
+    last_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class':'form-control'}))
+    username = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class':'form-control'}))
+    # last_login = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class':'form-control'}))
+    # is_superuser = forms.CharField(max_length=100, widget=forms.CheckboxInput(attrs={'class':'form-check'}))
+    # is_staff = forms.CharField(max_length=100, widget=forms.CheckboxInput(attrs={'class':'form-check'}))
+    # is_active = forms.CharField(max_length=100, widget=forms.CheckboxInput(attrs={'class':'form-check'}))
+    # date_joined = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class':'form-control'}))
+    # phone = forms.CharField(max_length=20, widget=forms.TextInput(attrs={'class':'form-control'}))
+    # address = forms.CharField(max_length=20, widget=forms.TextInput(attrs={'class':'form-control'}))
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email')
+        #fields = ('username', 'first_name', 'last_name', 'email', 'password', 'phone', 'address', 'last_login', 'is_superuser', 'is_staff', 'is_active', 'date_joined')
+
+
+class PasswordChangingForm(PasswordChangeForm):
+    old_password = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control', 'type':'password'}))
+    new_password1 = forms.CharField(max_length=100, widget=forms.PasswordInput(attrs={'class':'form-control', 'type':'password'}))
+    new_password2 = forms.CharField(max_length=100, widget=forms.PasswordInput(attrs={'class':'form-control', 'type':'password'}))
+
+    class Meta:
+        model = User
+        fields = ('old_password', 'new_password1', 'new_password2')
--- web_app/accounts/urls.py
+++ web_app/accounts/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
+#from .views import PasswordsChangeView
+from .views import UserEditView, PasswordsChangeView
+
 from django.contrib.auth import views as auth_view
-from .views import *
+from . import views
+from .views import register
 
 app_name = 'accounts'
 
@@ -10,4 +14,8 @@ urlpatterns = [
     path('logout/', auth_view.LogoutView.as_view(template_name = 'registration/logout.html'), name="logout"),
 
     path('register/', register, name='register'),
+    path('edit_profile/', UserEditView.as_view(), name='edit_profile'),
+
+    path('password/', PasswordsChangeView.as_view(template_name = 'registration/password_change.html'), name="password_change"),
+    path('password_success', views.password_success, name="password_success"),
 ]
